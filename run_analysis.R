d819dbf20d411ddcaf2a6688b4cd058e575ea1cd
spicykimchi <kyan@Kyan.lan>
2015-04-27 03:02:09 +0800

    debug

--- run_analysis.R
+++ run_analysis.R
@@ -7,29 +7,26 @@ trainDataX <- read.table("./UCI HAR Dataset/train/X_train.txt",header=FALSE)
 trainDataY <- read.table("./UCI HAR Dataset/train/y_train.txt",header=FALSE)
 trainDataSub <- read.table("./UCI HAR Dataset/train/subject_train.txt",header=FALSE)
 
-subject_all <- rbind(testDataSub, trainDataSub)
-features_data <- rbind(testDataX, trainDataX)
-
 # 3) Uses descriptive activity names to name the activities in the data set
 activities <- read.table("./UCI HAR Dataset/activity_labels.txt",header=FALSE,colClasses="character")
-testData_act$V1 <- factor(testData_act$V1,levels=activities$V1,labels=activities$V2)
-trainData_act$V1 <- factor(trainData_act$V1,levels=activities$V1,labels=activities$V2)
+testDataY$V1 <- factor(testDataY$V1,levels=activities$V1,labels=activities$V2)
+trainDataY$V1 <- factor(trainDataY$V1,levels=activities$V1,labels=activities$V2)
 
 # 4) Appropriately labels the data set with descriptive variable names. 
 features <- read.table("./UCI HAR Dataset/features.txt",header=FALSE,colClasses="character")
-colnames(testData)<-features$V2
-colnames(trainData)<-features$V2
-colnames(testData_act)<-c("Activity")
-colnames(trainData_act)<-c("Activity")
-colnames(testData_sub)<-c("Subject")
-colnames(trainData_sub)<-c("Subject")
-
+colnames(testDataX)<-features$V2
+colnames(trainDataX)<-features$V2
+colnames(testDataY)<-c("Activity")
+colnames(trainDataY)<-c("Activity")
+colnames(testDataSub)<-c("Subject")
+colnames(trainDataSub)<-c("Subject")
+ 
 # 1) Merges the training and the test sets to create one data set.
-testData<-cbind(testData,testData_act)
-testData<-cbind(testData,testData_sub)
-trainData<-cbind(trainData,trainData_act)
-trainData<-cbind(trainData,trainData_sub)
-bigData<-rbind(testData,trainData)
+testDataX<-cbind(testDataX,testDataY)
+testDataX<-cbind(testDataX,testDataSub)
+trainDataX<-cbind(trainDataX,trainDataY)
+trainDataX<-cbind(trainDataX,trainDataSub)
+bigData<-rbind(testDataX,trainDataX)
 
 # 2) Extracts only the measurements on the mean and standard deviation for each measurement. 
 bigData_mean<-sapply(bigData,mean,na.rm=TRUE)
